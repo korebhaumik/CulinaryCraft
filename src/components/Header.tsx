28fa27fb70a0a4c45d7ccde5f88246c94258fd0c
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-05 22:43:11 +0530

    Added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header({}: Props) {
   return (
     <header className="px-5 pt-5 fixed w-full z-50 max-w-4xl left-1/2 backdrop-blur-sm -translate-x-1/2">
       <nav className="flex justify-between mb-5 items-center">
-        <span className="text-2xl tracking-wide">CulinaryCraft</span>
+        <span className="text-2xl tracking-wide">🍽️ CulinaryCraft</span>
         <CommandCenter />
       </nav>
       <hr />
