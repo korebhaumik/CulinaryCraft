cd256185b810a48889161befdb8f36d7ae6c5d9c
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-06 15:44:05 +0530

    Title type fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "CuinaryCraft",
+  title: "CulinaryCraft",
   description: "A culinary AI that can help you with your cooking.",
 };
 
